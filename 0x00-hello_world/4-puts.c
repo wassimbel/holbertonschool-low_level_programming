46cbe13d664823a6f34c25dcf988ad396635b67a
wassimbel <wassim.belhedi1@gmail.com>
2020-02-05 14:06:39 +0100

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*main: is entry */
-/*return: give 0 */
+/**
+ * main - Entry pointA
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("/"Programming is like building a multilingual puzzle");
