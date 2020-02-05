1b53fe300db59d6f61bf957d9d43180e093b20e9
wassimbel <wassim.belhedi1@gmail.com>
2020-02-05 14:05:29 +0100

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-/*
-*jut
-*a
-*random
-*text
-*/
+/*main: is entry */
+/*return: give 0 */
 int main(void)
 {
-puts("/Programming is like building a multilingual puzzle");
+puts("/"Programming is like building a multilingual puzzle");
 return (0);
 }
 
