4600805948c01f484b16a98e6ce2458a85e7a80a
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 18:32:30 +0100

    exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - increment char
+ * d
+ * Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+ch++;
+}
+else 
+{
+ch++;
+}
+putchar('\n');
+return (0);
+}
+
+
