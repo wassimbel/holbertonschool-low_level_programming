50ccdf817fb869af3753b63a77a3d02b78784979
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 16:19:53 +0100

    print last digit of a stored number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,13 @@ if (j > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, j);
 }
-else if (j < 6 && j != 0)
+else if (j == 0)
 {
-printf("%d and is less than 6 and not 0\n", j);
+printf("Last digit of %d is %d and is zero\n", j);
 }
 else
 {
-printf("%d and is zero\n", j);
+printf("Last digit of %d is %d less than 6 and not 0\n", j);
 }
 return (0);
 }
