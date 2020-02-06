28d364069483a3c2da5b30f91505cb89cad798ad
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 16:37:19 +0100

    fixx

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ printf("Last digit of %d is %d and is greater than 5\n", n, j);
 }
 else if (j == 0)
 {
-printf("Last digit of %d is %d and is zero\n", n, j);
+printf("Last digit of %d is %d and is 0\n", n, j);
 }
 else
 {
-printf("Last digit of %d is %d less than 6 and not 0\n", n, j);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 }
 return (0);
 }
