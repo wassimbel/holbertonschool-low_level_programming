edc883a674bdc50a8f72fa4b1a94c64214098041
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 21:22:12 +0100

    combinations with comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n <= 57)
+{
+	putchar (n);
+	if (n != 57)
+	{
+		putchar(',');
+		putchar(' ');
+}
+
+	n++;
+}
+	putchar ('\n');
+
+	return (0);
+}
