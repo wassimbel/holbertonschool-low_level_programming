549e763ef1af45841c854e357dbd1a082f47b5d1
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 19:07:05 +0100

    print single digit numbers starting with 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,11 +3,11 @@
  * main - print single digit numbers starting from 0
  *d
  * Return: 0
- */
+ **/
 int main(void)
 {
 int i;
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 10; i++)
 {
 printf("%d\n", i);
 i++;
