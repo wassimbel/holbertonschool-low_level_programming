53892310bb3789ae68b4d75ff2c06ba55e74b99b
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 19:30:22 +0100

    print single digit numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers starting from 0
+ *d
+ * Return: 0
+ **/
+int main(void)
+{
+int i;
+for (i = 48; i < 57; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
