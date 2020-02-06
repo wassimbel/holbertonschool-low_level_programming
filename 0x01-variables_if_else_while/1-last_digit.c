bcf643c1f7b104aa7e700c0a4aca1ca81c4457e5
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 16:06:58 +0100

    print last digit of a stored number]

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,24 +8,23 @@
  */
 int main(void)
 {
-int n;
-int j;
-j = n % 10;
+int n, j;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+j = n % 10;
 printf("Last digit of");
 printf("%d", n);
 printf("is");
 if (j > 5)
 {
-printf("%d and is greater than 5", j);
+printf("%d and is greater than 5\n", j);
 }
 else if (j < 6 && j != 0)
 {
-printf("%d and is less than 6 and not 0 ", j);
+printf("%d and is less than 6 and not 0\n", j);
 }
 else
 {
-printf("%d and is zero", j);
+printf("%d and is zero\n", j);
 }
 }
