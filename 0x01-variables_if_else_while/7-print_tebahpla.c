9b14863e9f51a2c886ac10beaa4c0d42ea2e2709
wassimbel <wassim.belhedi1@gmail.com>
2020-02-06 19:39:44 +0100

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - increment char
+ * d
+ * Return: 0
+ */
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar ('\n');
+return (0);
+}
+
