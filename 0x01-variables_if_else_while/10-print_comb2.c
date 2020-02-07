2f4e28b6b08b451770e65ee096b0776755eee194
wassimbel <wassim.belhedi1@gmail.com>
2020-02-07 13:02:44 +0100

    print numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 100; i++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+if (i < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
