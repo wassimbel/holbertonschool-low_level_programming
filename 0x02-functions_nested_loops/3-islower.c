b78236f3479f6bfdc1e2a1e01fb228ec2f275292
wassimbel <wassim.belhedi1@gmail.com>
2020-02-11 20:30:56 +0000

    lowercase check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,6 @@
 /**
  * _islower - checks if letter is lower case
  * @c: checks to see if char is lower case
- *
- * Description: checks to see if char is lower case
- *
  * Return: Always 0
  */
 int _islower(int c)
