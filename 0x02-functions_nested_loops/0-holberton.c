497d1f1c572a1b60b4ddcf2ad771b6cff3eb2af9
wassimbel <wassim.belhedi1@gmail.com>
2020-02-11 13:22:45 +0100

    print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,22 @@
 #include "holberton.h"
 
 /**
- *main -entry
- *@void: no argument
- *Return:0
- **/
+ *main - main function
+ *
+ *Return: 0
+ */
 int main(void)
 {
-char holberon[] = "Holberton";
-int ch;
-for (ch = 0; ch < 9; ch++)
-{
-_putchar(holberton[ch]);
-}
-_putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
