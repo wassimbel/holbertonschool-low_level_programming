96468de919b826f9a236a9d47f4918f648704aad
wassimbel <wassim.belhedi1@gmail.com>
2020-02-11 20:59:36 +0000

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
- * _isalpha - checks if is alpha character
- * @c: checks if c is letter
+ * _isalpha - check c for alphabetic character
+ * @c: string
  *
- * Return: 1 if c is a letter 0 if not
- */
+ * Return: 1 if c is a letter, 0 if not
+*/
+
 int _isalpha(int c)
 {
 if (c <= 'z' && c >= 'a')
