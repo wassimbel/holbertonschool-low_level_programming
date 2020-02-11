e79f29cc6c5884ccbabb04249e1ae6023813f6ab
wassimbel <wassim.belhedi1@gmail.com>
2020-02-11 21:16:09 +0000

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,16 +9,16 @@
 
 int _isalpha(int c)
 {
-if (c <= 'z' && c >= 'a')
-{
-return (1);
-}
-else if (c <= 'Z' && c >= 'A')
-{
-return(1);
-}
-else
-{
-return (0);
-}
+	if (c <= 'z' && c >= 'a')
+	{
+		return (1);
+	}
+	else if (c <= 'Z' && c >= 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
