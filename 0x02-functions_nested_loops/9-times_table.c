7b905a54f962ae5a59b785352f62c94e7a9c5ae6
wassimbel <wassim.belhedi1@gmail.com>
2020-02-12 13:17:08 +0000

    print time tables 9 times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+*times_table - print the time table 9 times
+*
+*return: time table
+**/
+
+void times_table(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+x = i * j;
+if (j > 1)
+{
+_putchar(x + '0');
+_putchar(',');
+}
+else
+{
+if (x < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(x + '0');
+_putchar(',');
+}
+else if (x > 10)
+{
+_putchar(' ');
+_putchar(x / 10 + '0');
+_putchar(x % 10 + '0');
+}
+if (j != 9)
+}
+_putchar(',');
+}
+}
+_putchar('\n');
+}
+}
