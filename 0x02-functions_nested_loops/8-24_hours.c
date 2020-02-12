e8dc977689537a2c455189f78c429fdd595077dd
wassimbel <wassim.belhedi1@gmail.com>
2020-02-12 11:15:01 +0000

    print every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <"holberton.h">
+/**
+ * void jack_bauer - print every minute of the day
+ *
+ * @r: is the Variable of the function.
+ *
+ * Return: min
+ */
+
+void jack_bauer(void)
+{
+int h;
+int m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar(m / 10);
+_putchar(m % 10);
+}
+putchar(h / 10);
+putchar(h % 10);
+putchar(':');
+}
+}
