af27c21f1b13ddb51bfe94d09214970d2876f8ea
wassimbel <wassim.belhedi1@gmail.com>
2020-02-13 18:31:18 +0000

    draw straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*print_line - draw a straight line
+*@n:integer
+*
+**/
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+_putchar('_');
+_putchar('\n');
+}
+
