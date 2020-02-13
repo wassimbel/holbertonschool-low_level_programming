ba8d510de2dc8211c19f0113b5b002a539232b37
wassimbel <wassim.belhedi1@gmail.com>
2020-02-13 13:03:50 +0000

    print 10 times the numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,9 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 15; j++)
 {
 if (j < 10)
-{
-_putchar(j + '0');
-}
-else
-_putchar(j / 10 + '0' + j % 10 + '0');
-}
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
+}
 }
