cca6b6870f3de275a855bb3e4b15bc07421eebaa
wassimbel <wassim.belhedi1@gmail.com>
2020-02-13 11:40:49 +0000

    print numbers from 0 to 9 followed by new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 /**
 *print_numbers - print numbers from 0 to 9 followed bya new line
 *
-*return: return 0
+*
 **/
 void print_numbers(void)
-
 {
+int i;
 for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
 _putchar('\n');
 }
-return (0);
 }
