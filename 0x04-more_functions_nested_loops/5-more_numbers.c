cc4263b2c310b1e3a925a94bfc7071a7aebe0173
wassimbel <wassim.belhedi1@gmail.com>
2020-02-13 13:15:43 +0000

    print 10 times numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
+
+
 /**
 * more_numbers - print 10 times the numbers from 0 to 14
 *@void: no return
 *
 **/
+
 void more_numbers(void)
 {
 int i;
@@ -13,10 +16,11 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 15; j++)
 {
 if (j > 9)
+{
 _putchar(j / 10 + '0');
+}
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
 }
-
