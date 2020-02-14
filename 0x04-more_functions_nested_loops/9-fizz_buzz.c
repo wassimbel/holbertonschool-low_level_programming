54aa2d56dbe1472c4ac213fa3180536341a916de
wassimbel <wassim.belhedi1@gmail.com>
2020-02-14 12:45:18 +0100

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - print numbers 1 to 100
+ *
+ *Return: print to stdout numbers 1 to 100 except:
+ *replace multiples of three print Fizz
+ *replace multiples of five print Buzz
+ *numbers which are multiples of both three and five print FizzBuzz
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+
+else if (i % 3 == 0)
+
+printf("Fizz ");
+
+else if (i % 5 == 0)
+
+printf("Buzz ");
+
+else
+{
+printf("%d ", i);
+}
+printf("\n");
+}
+}
