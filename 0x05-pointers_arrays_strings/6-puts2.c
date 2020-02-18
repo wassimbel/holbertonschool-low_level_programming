2c37e472dc0f792a96fe923f3530f9c50745723c
wassimbel <wassim.belhedi1@gmail.com>
2020-02-18 17:23:56 +0000

    betty coding style fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,9 @@
   */
 void puts2(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		if (str[i] % 2 == 0)
-			_putchar(str[i]);
-	_putchar('\n');
+int i;
+for (i = 0; str[i] != '\0'; i++)
+if (str[i] % 2 == 0)
+_putchar(str[i]);
+_putchar('\n');
 }
