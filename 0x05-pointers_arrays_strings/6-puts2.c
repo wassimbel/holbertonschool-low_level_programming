041a2188e20c01b81a24e99fb951f2fecff168c4
wassimbel <wassim.belhedi1@gmail.com>
2020-02-18 17:37:37 +0000

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 /**
-  * puts2 - prints one character out of two
-  * @str: takes in a string
-  * Return: void
-  */
+*puts2 - prints one character out of two
+*@str: takes in a string
+*Return: void
+*/
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i++)
-if (str[i] % 2 == 0)
+i = 0;
+while(*(str + i) != '\0')
+{
+if (*(str + i) % 2 == 0)
 _putchar(str[i]);
+i++;
+}
 _putchar('\n');
 }
