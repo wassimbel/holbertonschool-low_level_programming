35fcc489dfc61fa8a9c9919925fb0fdb1e42a3b3
wassimbel <wassim.belhedi1@gmail.com>
2020-02-18 18:06:34 +0000

    fixing

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,16 @@
  *Return: void
  *
 **/
+
 void puts2(char *str)
 {
 int i;
 i = 0;
-while (*(str + i) != '\0')
+while (str[i] != '\0')
 {
-if (*(str + i) % 2 == 0)
+if (i % 2 == 0)
 _putchar(str[i]);
-i++;
+i+=2;
 }
 _putchar('\n');
 }
