66e2635c1561010418b33397807f510c5cad3c81
wassimbel <wassim.belhedi1@gmail.com>
2020-02-18 17:06:21 +0000

    print every other character of a string.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,13 +3,14 @@
 *puts2 - print every other charracter of a string
 *@str: string
 **/
+
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] % 2 == 0)
-_putchar(str[i]);
-}
-_putchar('\n');
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
 }
