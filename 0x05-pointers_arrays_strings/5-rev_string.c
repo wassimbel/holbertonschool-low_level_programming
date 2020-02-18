b471d6dcffd40a06a8dc0eb1923080061681913d
wassimbel <wassim.belhedi1@gmail.com>
2020-02-18 16:19:30 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*rev_string - reverses a string
+*@s:string
+**/
+void rev_string(char *s)
+{
+int i = 0, j = 0, len = 0, e = 0;
+char tmp;
+
+	for (j = 0; s[j] != '\0';  j++)
+	len = j + 1;
+	for (i = len - 1; i >= len / 2; i--)
+	{
+	tmp = s[e];
+	s[e] = s[i];
+	s[i] = tmp;
+	e++;
+  	}
+
+}
