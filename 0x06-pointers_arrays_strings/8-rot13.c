c2ba5e419f0f96a090fabc440e557170e41aac86
wassimbel <wassim.belhedi1@gmail.com>
2020-02-20 22:28:20 +0000

    encode a string into rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,23 +7,21 @@
 char *rot13(char *s)
 {
 int i, j;
-
+int c = 0;
 char *a = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
 char *b = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; a[j] != '\0'; j++)
+for (j = 0; a[j] != '\0' && c == 0; j++)
 {
 if (s[i] == a[j])
 {
 s[i] = b[j];
-}
-else
-{
-s[i] = a[j]
+n = 1;
 }
 }
+n = 0;
 }
 return (s);
 }
