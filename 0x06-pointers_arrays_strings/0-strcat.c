9c2d880061ff6507d2f4df8df88a0f0465b7c906
wassimbel <wassim.belhedi1@gmail.com>
2020-02-20 12:51:20 +0000

    concatenate two string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,10 @@ int i = 0, j;
 for (i = 0; dest[i] != '\0'; i++)
 ;
 j = 0;
-for (j = 0; src[j] != '\0'; j++)
+for (j = 0; src[j] != '\0'; i++, j++)
 {
-dest[i + j] = src[j];
+dest[i] = src[j];
 }
+dest[i] = '\0';
 return (dest);
 }
