b661f2263864824d7830631112bb7dd184f24a5e
wassimbel <wassim.belhedi1@gmail.com>
2020-02-20 12:44:24 +0000

    concatenate two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+**_strcat - concatenates dest and src
+*@dest: char
+*@src: char
+*Return:pointer to resulting string dest
+**/
+
+char *_strcat(char *dest, char *src)
+{
+
+int i = 0, j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+j = 0;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i + j] = src[j];
+}
+return (dest);
+}
