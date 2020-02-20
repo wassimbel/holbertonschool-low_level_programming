1e34d87be7788e54a4d8be5257bfad27b81d502f
wassimbel <wassim.belhedi1@gmail.com>
2020-02-20 15:32:23 +0000

    copy a string using strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0, j;
 for (i = 0; src[i] != '\0'; i++)
-for (j = 0; src[j] != '\0'; j++)
+for (j = 0; j < n && src[j] != '\0'; j++)
 dest[j] = src[j];
 for (; i < n; i++ )
 dest[i] = '\0';
