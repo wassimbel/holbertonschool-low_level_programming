8d059aa727495bc7c9049e7cd18af4c1d77fe286
wassimbel <wassim.belhedi1@gmail.com>
2020-02-21 20:37:09 +0000

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+**cap_string - capitalize all words of a string
+*@s:string
+*Return:string
+**/
+char *cap_string(char *s)
+{
+int i = 0, j;
+char a[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+if (s[0] >= 'a' && s[0] <= 'z')
+
+s[0] = s[0] - 32;
+for (i = 0; s[i] != '\0'; i++)
+
+j = 0;
+
+for (j = 0; a[j] != '\0' && s[i] != a[i]; j++)
+{
+
+if (s[i] == a[i] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+
+s[i + 1] = s[i + 1] - 32;
+break;
+
+}
+
+return (s);
+
+}
