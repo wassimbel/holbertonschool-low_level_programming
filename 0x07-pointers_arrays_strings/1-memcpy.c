ac438a39ac39f6de1ce4439c3f2c0e94af845fb3
wassimbel <wassim.belhedi1@gmail.com>
2020-02-24 11:25:28 +0000

    copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,8 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i, j;
+unsigned int i;
 for (i = 0; i < n; i++)
-for (j = 0; j < n; j++)
-{
-dest[i] = src[j]
-}
+dest[i] = src[i]
 return (dest);
 }
