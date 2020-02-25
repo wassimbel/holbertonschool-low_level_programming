045b56ee1de5c3d030f613f0c2a6d76757a8c1e8
wassimbel <wassim.belhedi1@gmail.com>
2020-02-25 18:08:32 +0000

    gets the lenght of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+
+/**
+*int_strspn - gets the legnth of a prefix substring
+*@s: string
+*@accept: string
+**/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j, e;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+e++;
+break;
+}
+if (s[i] != accept[j])
+break;
+}
+}
+return (e);
+}
+
