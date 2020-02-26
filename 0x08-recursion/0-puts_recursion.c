a17a4aa2d898082927799bc42388c1ffbea97be6
wassimbel <wassim.belhedi1@gmail.com>
2020-02-26 10:48:55 +0000

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+*_puts_recursion - print a string followed by a new line
+*@s:string
+**/
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+if (*s != '\0')
+_putchar(*s);
+_puts_recursion(s + 1);
+}
