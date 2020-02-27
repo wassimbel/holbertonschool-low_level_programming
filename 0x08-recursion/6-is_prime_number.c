e92724a26a0cc832180d11bf2566a6bf1a0d9276
wassimbel <wassim.belhedi1@gmail.com>
2020-02-27 20:17:51 +0000

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+
+/**
+*is_prime_number - returns a prime number
+*@n:integer
+*Return:integer
+*/
+
+int is_prime_number(int n)
+{
+if (n == 1 || n < 0)
+return (0);
+return (_prime_num(n, 2));
+}
+
+/**
+*helper_prime - helps find the prime number
+*@i:integr
+*@n:integer
+*Return:integer
+*/
+
+int _prime_num(int n, int i)
+{
+if (n == i)
+{
+return(1);
+}
+else if (n % i == 0)
+{
+return (0);
+}
+return (_prime_num(n, i + 1));
+}
