50ff9d4ff3b43a24512bdc4a0f6c17087dd521ff
wassimbel <wassim.belhedi1@gmail.com>
2020-03-01 11:15:03 +0000

    print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include "holberton.h"
 
-int main(int argc, char* argv[])
+
+/**
+*main - main function
+*@argc: number of arguments
+*@argv: array of strings
+*Return: return 0
+**/
+
+int main(int argc, char *argv[])
 {
 int i;
-for(i = 0; i < argc; i++)
+for (i = 0; i < argc; i++)
 printf("%s\n", argv[0]);
-return(0);
+return (0);
 }
