ca4f1364344a59f200812dacf3efe7c20cf2e220
wassimbel <wassim.belhedi1@gmail.com>
2020-03-01 16:50:27 +0000

    multiply numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - main function
+*@argc: number of arguments
+*@argv: array of strings
+*Return: return 1 if argc < 2 else return multiplication of two numbers
+**/
+
+int main(int argc, char *argv[])
+{
+int i, mul = 1;
+
+if (argc < 2)
+{
+printf("Error\n");
+return (0);
+}
+else
+{
+for (i= 1; i < argc; i++)
+mul = mul * atoi(argv[i]);
+printf("%d\n", mul);
+return (0);
+}
+}
