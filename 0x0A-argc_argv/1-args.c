3e18805fe89e6d03f44fb32621125753515783c4
wassimbel <wassim.belhedi1@gmail.com>
2020-03-01 12:08:10 +0000

    print number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+*main - main function
+*@argc: number of arguments
+*@argv: array of strings
+*Return: return 0
+**/
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%d", argc);
+return (0);
+}
