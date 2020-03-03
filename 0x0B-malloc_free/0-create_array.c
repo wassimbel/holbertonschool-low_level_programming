64047294a329fbf7a4df1db271a919dccb2c0d1b
wassimbel <wassim.belhedi1@gmail.com>
2020-03-03 14:31:14 +0000

    creates an arrays of chars with initialized with specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*create_array - func create an array of chars, initialized with specific char
+*@size: size of the array
+*@c: starting character
+*Return: pointer to array
+*/
+char *create_array(unsigned int size, char c)
+{
+char *ptr;
+unsigned int i;
+
+if (size == 0)
+return (NULL);
+ptr = malloc(size * sizeof(char));
+if (ptr == NULL)
+return (NULL);
+else
+{
+for
+(i = 0; i < size; i++)
+ptr[i] = c;
+return (ptr);
+}
+}
+
