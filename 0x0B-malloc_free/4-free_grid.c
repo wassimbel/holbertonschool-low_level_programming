587dab36a20221ab4deed0dec6c7475bc9a18633
wassimbel <wassim.belhedi1@gmail.com>
2020-03-03 22:09:06 +0000

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+*free_grid - free grid
+*@grid:grid
+*@height:height
+*Return:return 0
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+
+free(grid);
+}
