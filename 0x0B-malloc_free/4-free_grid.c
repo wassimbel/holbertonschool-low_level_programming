7cd38b4284791f1dfbc0562d274b7360d2349238
wassimbel <wassim.belhedi1@gmail.com>
2020-03-03 22:17:34 +0000

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,15 +1,22 @@
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
 /**
-*free_grid - free grid
-*@grid:grid
-*@height:height
-*Return:return 0
-*/
+  * free_grid - free grid space
+  * @grid: takes in the grid from #3
+  * @height: takes in the height
+  */
 void free_grid(int **grid, int height)
 {
-int i;
-for (i = 0; i < height; i++)
-free(grid[i]);
-free(grid);
+	int i;
+
+	if (grid == NULL || grid == 0)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+			free(grid[i]);
+		free(grid);
+	}
 }
