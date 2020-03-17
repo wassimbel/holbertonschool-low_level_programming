f38aa119d2bb19367648cdec65c25f2108d52a8a
wassimbel <wassim.belhedi1@gmail.com>
2020-03-17 22:24:48 +0000

    new name of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ float age;
 char *owner;
 };
 
-typedef struct dog_t dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
