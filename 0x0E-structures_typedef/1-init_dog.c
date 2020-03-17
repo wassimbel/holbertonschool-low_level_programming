6baba3ea782e519c74308e36de321e4c68a0e994
wassimbel <wassim.belhedi1@gmail.com>
2020-03-17 20:14:44 +0000

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+/**
+* init_dog - struct
+*@d: struct
+*@name: string
+*@age: number
+*@owner: string
+* Return:0.
+*/
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+if (d != NULL)
+{
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+}
+}
