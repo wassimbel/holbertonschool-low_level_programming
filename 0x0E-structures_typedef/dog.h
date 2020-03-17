33c4047d1fcf5207d8a0e8c07633fbe41b0cb973
wassimbel <wassim.belhedi1@gmail.com>
2020-03-17 19:25:42 +0000

    first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
