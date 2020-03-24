ba088e3ab8d6ce3837f5f54a4a8493b87afeec8b
wassimbel <wassim.belhedi1@gmail.com>
2020-03-24 21:28:15 +0000

    just to try

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,22 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
-
 /**
- * free_listint2 - free list_t
- * @head: pointer to pointer the head of the list
+ * free_listint2 - check the code.
+ * @head: list
+ * Return: Always .
  */
 
 void free_listint2(listint_t **head)
 {
-listint_t *new;
-if (head == NULL)
-return;
+	listint_t *tmp, *h;
 
-if (head != NULL && *head != NULL)
+	if (head)
 {
-new = *head;
-*head = new->next;
-free(new);
+	h = *head;
+	if (!h)
+	return;
+		while (h)
+		{
+		tmp = h->next;
+		free(h);
+		h = tmp;
+		}
+	*head = NULL;
+}
 }
 
-}
