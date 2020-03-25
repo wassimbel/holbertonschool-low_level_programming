8060ce9faecca39883ba4fb2608f20d7efd8bf78
wassimbel <wassim.belhedi1@gmail.com>
2020-03-25 18:05:29 +0000

    add optional

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 
+
+void __attribute__((constructor)) prevmain(void);
+
+
 /**
  * prevmain - function that is called before main
  * @void: no arg
@@ -7,6 +11,6 @@
 
 void prevmain(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
