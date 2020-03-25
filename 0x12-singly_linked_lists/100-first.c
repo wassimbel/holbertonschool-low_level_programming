be61a17781e66956ebf2d65f396020c9d7861959
wassimbel <wassim.belhedi1@gmail.com>
2020-03-25 18:02:48 +0000

    add optional

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * prevmain - function that is called before main
+ * @void: no arg
+ */
+
+void prevmain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
