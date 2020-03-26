54221705639f82a8657ec4e10a65188d551c6352
wassimbel <wassim.belhedi1@gmail.com>
2020-03-26 21:07:47 +0000

    number bits you would need to flip to get from one num to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * flip_bits - number of bits you d need to flip to get from number to another
+  * @n: integer
+  * @m: integer to compare
+  * Return: number of bits to flip
+  */
+
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int count = 0, diff = n ^ m;
+
+while (diff != 0)
+{
+if (diff & 1)
+count += 1;
+
+diff = diff >> 1;
+}
+return (count);
+}
