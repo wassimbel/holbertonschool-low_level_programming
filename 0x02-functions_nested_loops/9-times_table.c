be91ad51c7162bf96d449a6d923caceefde36410
wassimbel <wassimbel.belhedi1@gmail.com>
2020-04-16 15:09:39 +0000

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,39 +7,33 @@
 
 void times_table(void)
 {
-int i;
-int j;
-int x;
+int i, j, x = 0;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
 x = i * j;
-if (j < 1)
+if (j == 0)
 {
 _putchar(x + '0');
-_putchar(',');
 }
-if (x < 10)
+else if (x < 10)
 {
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(x + '0');
-_putchar(',');
 }
-else if (x > 10)
+else if (x >= 10)
 {
+_putchar(',');
 _putchar(' ');
 _putchar(x / 10 + '0');
 _putchar(x % 10 + '0');
-_putchar(',');
 }
-if (j != 9)
-{
-_putchar(',');
 }
 _putchar('\n');
 }
 }
-}
+
 
