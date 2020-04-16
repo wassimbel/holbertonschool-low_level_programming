3fd87e3a31f719f730226d3737edb8db73eb963a
wassimbel <wassimbel.belhedi1@gmail.com>
2020-04-16 20:51:26 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - main func
+  * @void: no args
+  * Return: 0
+  */
+
+int main(void)
+{
+long i, num = 612852475143;
+
+for (i  = 2; i <= num; i++)
+{
+if (num % i == 0)
+{
+num = num / i;
+i--;
+}
+}
+printf("%lu\n", i);
+return (0);
+}
+
