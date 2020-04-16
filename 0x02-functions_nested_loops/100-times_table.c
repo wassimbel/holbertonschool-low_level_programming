7c47dcd65e32d04b167d4fd87a7212fd728434fc
wassimbel <wassimbel.belhedi1@gmail.com>
2020-04-16 15:06:10 +0000

    fix betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,17 +1,14 @@
 #include "holberton.h"
 
- /**
-  * print_times_table - prints the times table starting with 0
+/**
+  * print_times_table - prints the times table
   * @n: integer
   */
-
 void print_times_table(int n)
 {
 int i, j, x;
-
 if (n > 15 || n < 0)
 return;
-
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -21,7 +18,6 @@ if (j == 0)
 {
 _putchar(x + '0');
 }
-
 else if (x < 10)
 {
 _putchar(',');
@@ -30,7 +26,6 @@ _putchar(' ');
 _putchar(' ');
 _putchar(x + '0');
 }
-
 else if (x >= 10 && x < 100)
 {
 _putchar(',');
@@ -39,7 +34,6 @@ _putchar(' ');
 _putchar(x / 10 + '0');
 _putchar(x % 10 + '0');
 }
-
 else
 {
 _putchar(',');
