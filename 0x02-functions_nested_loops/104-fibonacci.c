fd4b88ed3f3298b305e5f08eb6561a611d20a9a9
wassimbel <wassim.belhedi1@gmail.com>
2020-04-17 16:35:29 -0400

    add fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,11 @@ double x = 0, y = 0, z = 0;
 printf("%i, %i, ", 1, 2);
 x = 1;
 y = 2;
-for (i = 3 ; i <= 98 ; ++i)
+for (i = 3 ; i <= 98 ; i++)
 {
 z = x + y;
-
 if (i != 98)
 printf("%.0f, ", z);
-
 else
 printf("%.0f", z);
 x = y;
