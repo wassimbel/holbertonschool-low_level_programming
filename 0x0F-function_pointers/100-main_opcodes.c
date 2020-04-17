7b11b98e1733d959573b03f972e63486c19b5e54
wassimbel <wassim.belhedi1@gmail.com>
2020-04-17 17:24:17 -0400

    fix

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -main func
+ * @argc: args number
+ * @argv: args content
+ * Return: 0
+ */
+
+
+int main(int argc, char **argv)
+{
+int i, x;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+x = atoi(argv[1]);
+if (x < 0)
+{
+printf("Error\n");
+return (2);
+}
+for (i = 0; i < (x - 1); i++)
+{
+if (x != 0)
+printf("%02hhx ", ((char *)main)[i]);
+}
