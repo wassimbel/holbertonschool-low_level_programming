de90dd597d3a3bb51101df6b58ff284a4409b679
wassimbel <wassim.belhedi1@gmail.com>
2020-04-17 16:28:09 -0400

    add fix

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - main func
+  * @void: no args
+  * Return: 0
+  */
+
+int main(void)
+{
+int i;
+double x = 0, y = 0, z = 0;
+printf("%i, %i, ", 1, 2);
+x = 1;
+y = 2;
+
+for (i = 3 ; i < 99 ; i++)
+{
+z = x + y;
+
+if (i != 98)
+printf("%.0f, ", z);
+
+else
+printf("%.0f", z);
+x = y;
+y = z;
+}
+return (0);
+}
