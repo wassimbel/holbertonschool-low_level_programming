772a5d22aee78f872937554487cb308b082400a9
wassimbel <wassim.belhedi1@gmail.com>
2020-04-17 16:48:34 -0400

    fix

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ * @void: no args
+ * Return: 0
+ **/
+
+int main(void)
+{
+int i, j, e;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = i + 1; j <= '9'; j++)
+{
+for (e = j + 1; e <= '9'; k++)
+{
+putchar(i);
+putchar(j);
+putchar(e);
+
+if (i != '7' || j != '8' || e != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
