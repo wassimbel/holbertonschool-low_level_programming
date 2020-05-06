befbb94498d81611a5f61fc18f54521430b6ed17
wassimbel <wassim.belhedi1@gmail.com>
2020-05-06 06:10:45 -0400

    fix

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,8 +1,5 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-
-
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,5 +20,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 #endif /* _HOLBERTON_H_  */
