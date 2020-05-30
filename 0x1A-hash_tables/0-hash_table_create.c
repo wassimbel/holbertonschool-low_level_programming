65925d7d81770d5d7058756c63eeda61358baf93
wassimbel <wassim.belhedi1@gmail.com>
2020-05-30 16:53:26 -0400

    terrible fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,11 @@
 #include "hash_tables.h"
 
-
 /**
- * hash_table_create - create a hash table
- * @size: size of the array
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
  *
- * Return: a pointer to the newly created hash table
- **/
-
+ * Return: a pointer to the hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
