394a2f958a5d60edcaf300fb714dc4545716679a
wassimbel <wassim.belhedi1@gmail.com>
2020-05-30 23:29:52 -0400

    fix betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - retrieves an element to the hash table
+ * hash_table_get - retrieves an element to the hash table
  * @ht: the hash table
  * @key: the key
  * Return: the value of the key, or Null if key not found
@@ -10,22 +10,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 
-        hash_node_t *new_node;
-        unsigned long int index;
+	hash_node_t *new_node;
+	unsigned long int index;
 
-        if (!ht || !key)
-                return (NULL);
-        index = key_index((const unsigned char *)key, ht->size);
-        new_node = ht->array[index];
-        if (!new_node)
-                return (NULL);
+	if (!ht || !key)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = ht->array[index];
+	if (!new_node)
+	return (NULL);
 
-        while (new_node)
-        {
-                if (strcmp(new_node->key, key) == 0)
-                        return (new_node->value);
-                new_node = new_node->next;
-        }
-        return (NULL);
+	while (new_node)
+	{
+	if (strcmp(new_node->key, key) == 0)
+		return (new_node->value);
+		new_node = new_node->next;
+	}
+	return (NULL);
 }
 
