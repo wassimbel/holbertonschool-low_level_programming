85ea82cc460e5c6171d961b12cb984dfffbb92aa
wassimbel <wassim.belhedi1@gmail.com>
2020-05-30 20:14:54 -0400

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
 	if (!new_node->key || !new_node->value)
 	{
 		free(new_node->key);
