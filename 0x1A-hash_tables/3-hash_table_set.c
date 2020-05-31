6026932e0c35e2e6214a21c466f17166fcc9eca3
wassimbel <wassim.belhedi1@gmail.com>
2020-05-30 20:08:36 -0400

    first fix

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 1 if succeeded, 0 otherwise
+ **/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	hash_node_t *node, *new_node;
+	unsigned long int index;
+
+	if (!ht || !key)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (!node || !node->key)
+		return (0);
+	while (node && node->key)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	if (!new_node->key)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
