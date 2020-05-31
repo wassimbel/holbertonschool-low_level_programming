2fbebd851ce0cd458481ef28a01ecd141cdecd04
wassimbel <wassim.belhedi1@gmail.com>
2020-05-30 20:48:32 -0400

    value can be empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	if (!new_node->key || !new_node->value)
+	if (!new_node->key)
 	{
 		free(new_node->key);
 		free(new_node->value);
