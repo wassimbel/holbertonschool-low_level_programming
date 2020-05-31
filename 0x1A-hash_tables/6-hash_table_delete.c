0048df4a0010b72ac95a958cb804132c634e41dd
wassimbel <wassim.belhedi1@gmail.com>
2020-05-31 16:57:24 -0400

    update

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ *
+ *
+ **/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *tmp = NULL;
+	unsigned long int i = 0;
+
+	if (!ht)
+		return;
+	node = ht->array[i]
+	while (i < ht->size)
+	{
+		while (node)
+		{
+			tmp = node->next;
+			free(node->value);
+			free(node->key);
+			free(node);
+			node = tmp;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
+
