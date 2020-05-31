87c8913625a88e33c7eef064eeb17167e659b281
wassimbel <wassim.belhedi1@gmail.com>
2020-05-30 20:13:54 -0400

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,6 +43,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
